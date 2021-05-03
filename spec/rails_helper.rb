10bc0b8c9068fdf36467ab590d6cfa257f3fc384
root <ibaltaza@uccs.edu>
2021-05-03 23:16:56 +0000

    Commit Message

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,4 +72,6 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, :type => :controller
   config.include FactoryBot::Syntax::Methods
   config.extend ControllerMacros, :type => :controller
+
+  config.include ControllerMacros
 end
